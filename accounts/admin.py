344f002407b322448e99352ca0703b22e73d1f57
dedcode <difallah@gmail.com>
2013-09-26 20:12:42 +0200

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from accounts.models import Batch,Task
+from accounts.models import Batch,Task, UserProfile
 
 class TaskAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -10,12 +10,17 @@ class TaskInline(admin.StackedInline):
     extra = 3
 class BatchAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,     {'fields': ['value','repetition','numtask','bclass','name']}),
+        (None,     {'fields': ['name','bclase','done','value','repetition','numtask']}),
     ]
     def formfield_for_choice_field(self, db_field, request, **kwargs):
         if db_field.name == "bclass":
             kwargs['class'] = (('classify','classify'),('extract','extract'),('curate','curate'),('data','data'),('study','study'))
         return super(BatchAdmin, self).formfield_for_choice_field(db_field, request, **kwargs)
     inlines = [TaskInline]
+
+class UserProfileAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['user','credit','score']}),
+    ]
 
 admin.site.register(Batch, BatchAdmin)
