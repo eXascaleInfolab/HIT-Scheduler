7e33cbf241c8626a7c7ae92bb4f9b151dc283d21
dedcode <difallah@gmail.com>
2014-03-28 18:12:54 +0100

    fix

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django import forms
-from captcha.fields import ReCaptchaField
 
 class UserProfile(models.Model):
 	user = models.ForeignKey(User, unique=True)
