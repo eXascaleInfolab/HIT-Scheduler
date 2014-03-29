8be8cd80659b00ae80c30a82de1892eb3fc99839
dedcode <difallah@gmail.com>
2014-03-29 16:18:35 +0100

    dist

--- train/settings.py
+++ train/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 
 # DATABASES = {'default': dj_database_url.config(default='postgres://localhost/train')}
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost/train')}
 
 # DATABASES = {
 #     'default': {
