850001e59ba7d24ed9563dbb4cf2efe2ee9637f7
dedcode <difallah@gmail.com>
2014-03-29 17:38:22 +0100

    dist

--- cus_reset.sh
+++ cus_reset.sh
@@ -1,3 +1,3 @@
-./reset.sh
-./manage.py loaddata fixtures/customer.json
-#python manage.py runserver 0.0.0.0:7000
+heroku pg:reset DATABASE_URL --confirm ancient-bayou-8476
+heroku run python manage.py syncdb
+heroku run python manage.py loaddata fixtures/customer.json
