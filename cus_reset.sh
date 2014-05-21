8beee94ee3af4836e02014d16d16d50e01b59dc1
dedcode <difallah@gmail.com>
2014-05-21 22:38:18 +0200

    modif

--- cus_reset.sh
+++ cus_reset.sh
@@ -1,3 +1,3 @@
-heroku pg:reset DATABASE_URL --confirm guarded-caverns-1880 
+heroku pg:reset DATABASE_URL --confirm fast-ocean-3359.git 
 heroku run python manage.py syncdb
-heroku run python manage.py loaddata fixtures/er.json
+heroku run python manage.py loaddata short_fixtures/fillbatch.json
