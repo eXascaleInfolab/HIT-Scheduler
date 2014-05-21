5d531c007408173fbc449555eb01de8837d07a76
dedcode <difallah@gmail.com>
2014-05-21 22:39:57 +0200

    trace

---
+++ run_trace.py
@@ -0,0 +1,15 @@
+import time
+from subprocess import call
+
+f = open("scripts/trace.txt", "r")
+cnt = 1
+for line in f.readlines():
+	# run the command
+	call(["heroku run python manage.py loaddata short_fixtures/b"+str(cnt)+".json"],shell=True)
+	cnt = cnt + 1
+	s = int(line.strip())
+	print "sleeping", s
+	# sleep for the next one
+	# time.sleep(s)
+	
+f.close()
