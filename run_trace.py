d76c04605b2fd49d2f351a3f6d9eacaaaee48458
dedcode <difallah@gmail.com>
2014-06-01 23:19:37 +0200

    re-organize the fixture files

--- run_trace.py
+++ run_trace.py
@@ -1,15 +1,18 @@
 import time
 from subprocess import call
+import os,sys
 
 f = open("scripts/trace.txt", "r")
 cnt = 1
 for line in f.readlines():
 	# run the command
+	print 'Executing', cnt
 	call(["heroku run python manage.py loaddata short_fixtures/b"+str(cnt)+".json"],shell=True)
+	print('\a')
 	cnt = cnt + 1
 	s = int(line.strip())
 	print "sleeping", s
 	# sleep for the next one
-	time.sleep(s)
+	#time.sleep(s)
 	
 f.close()
